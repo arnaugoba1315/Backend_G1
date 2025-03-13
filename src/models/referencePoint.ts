a019ea7c0f9c08434f6b8fed153b8a6ff3e5002a
JanaCorsellas <jana.corsellas@estudiantat.upc.edu>
2025-03-13 08:24:48 +0100

    referencePoint model

---
+++ src/models/referencePoint.ts
@@ -0,0 +1,44 @@
+import mongoose, {Schema} from "mongoose";
+const referencePointSchema = new mongoose.Schema({
+    name :{
+        type: String,
+        require: false
+    },
+    activity :{
+        type: Schema.Types.ObjectId,
+        require: true
+    },
+    latitude :{
+        type: Number,
+        require: true
+    },
+    longitude :{
+        type: Number,
+        require: true
+    },
+    timestamp :{
+        type: Date,
+        require: true
+    },
+    altitude :{
+        type: Number,
+        require: false
+    },
+    heartRate :{
+        type: Number,
+        require: false
+    }
+});
+
+export interface IReferencePoint{
+    name?: string;
+    activity: mongoose.Types.ObjectId[];
+    latitude: number;
+    longitude: number;
+    timestamp: Date;
+    altitude?: number;
+    heartRate?: number;
+}
+
+const ReferencePoint = mongoose.model('ReferencePoint', referencePointSchema);
+export default ReferencePoint;
