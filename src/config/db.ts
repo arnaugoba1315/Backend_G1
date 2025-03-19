a2c8d445b57390fffaefc1dcdc4fafed1ce5c878
JuanAntonio234 <127333032+JuanAntonio234@users.noreply.github.com>
2025-03-19 13:13:35 +0100

    separada config swagger y db

--- src/config/db.ts
+++ src/config/db.ts
@@ -1,15 +1,40 @@
-import mongoose from "mongoose";
+import mongoose, { Mongoose } from "mongoose";
+import {Express} from "express";
+import dotenv from "dotenv";
 
-const conectarDB = async () => {
-    try {
+dotenv.config();
+
+const connectDatabase = async () => {
+    try{
         mongoose.set('strictQuery', true);
+        await mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/ProyectoEA_bd');
+        console.log('Conexión exitosa a MongoDB');
 
-        const conexion = await mongoose.connect(process.env.MONGO_URI || "mongodb://localhost:27017/BackendProjecte");
-        console.log(`Connected to MongoDB successfully: ${conexion.connection.host}`);
-    } catch (error) {
-        console.error("Failed to connect to MongoDB", error);
-        process.exit(1); 
-    }
+  
+// Manejadores de eventos para la conexión a MongoDB
+mongoose.connection.on('connected', () => {
+    console.log('Mongoose conectado a la base de datos');
+    });
+    
+    mongoose.connection.on('error', (err) => {
+    console.error('Error en la conexión de Mongoose:', err);
+    });
+    
+    mongoose.connection.on('disconnected', () => {
+    console.log('Mongoose desconectado de la base de datos');
+    });
+    
+    // Manejador para cerrar la conexión cuando se detiene la aplicación
+    process.on('SIGINT', async () => {
+    await mongoose.connection.close();
+    console.log('Conexión de Mongoose cerrada debido a la terminación de la aplicación');
+    process.exit(0);
+    });
+}
+catch(error){
+  console.error('Error de conexión a MongoDB:', error);
+  console.error('Asegúrate de que MongoDB esté ejecutándose en tu sistema');
+}
 };
 
-export default conectarDB;
+export default connectDatabase;
