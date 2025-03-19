76c46f79e290cb1ecef599af76898eccca8e47a0
Pol Altes Tomas <pol.altes@estudiantat.upc.edu>
2025-03-19 12:33:09 +0100

    Song Services

--- src/services/songService.ts
+++ src/services/songService.ts
@@ -1,6 +1,30 @@
 import Song, {ISong} from '../models/song.js';
+import {Schema} from 'mongoose';
+
+//createSong , getAllSongs, getSongById, updateSong, deleteSong
 
 export const createSong = async (song:ISong) => {
     const newSong = new Song(song);
     return await newSong.save();
 };
+
+export const getAllSongs = async () => {
+    return await Song.find();
+};
+
+export const getSongById = async (id:Schema.Types.ObjectId) => {
+    return await Song.findById(id);
+};
+
+export const getSongByName = async (name:string) => {
+    return await Song.find(title:name);
+};
+
+export const updateSong = async (id:Schema.Types.ObjectId, song:ISong) => {
+    return await Song.findByIdAndUpdate;
+};
+
+export const deleteSong = async (id:Schema.Types.ObjectId) => {
+    return await Song.findByIdAndDelete(id);
+};
+
