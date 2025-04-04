09833392c0789ca36c983ef436468f0b4da588f0
arnaugoba1315 <arnau.gonzalez.barrios@estudiantat.upc.edu>
2025-04-05 00:13:01 +0200

    RUTA REGISTER CREADA
    FUNCIONA REGISTER DESDE WEB

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -296,6 +296,37 @@ router.delete('/:id', userController.deleteUser);
  */
 router.put('/:id/toggle-visibility', userController.toggleUserVisibility);
 
-
+/**
+ * @openapi
+ * /api/users/register:
+ *   post:
+ *     summary: Register a new user
+ *     tags: [Users]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             required:
+ *               - username
+ *               - email
+ *               - password
+ *             properties:
+ *               username:
+ *                 type: string
+ *               email:
+ *                 type: string
+ *               password:
+ *                 type: string
+ *     responses:
+ *       201:
+ *         description: User registered successfully
+ *       400:
+ *         description: Username already exists
+ *       500:
+ *         description: Error creating user
+ */
+router.post('/register', userController.createUser);
 
 export default router;
